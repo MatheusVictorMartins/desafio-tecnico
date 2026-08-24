ef3f2a81db9f1520e28b974698784d988af8dc69
Matheus <matheusvictor.profissional@gmail.com>
2026-08-24 18:25:01 -0300

    feat(backend/04): adicionado migration de proprietario e entidade do mesmo

--- backend/src/main/java/br/com/webgis/imovel/ImovelService.java
+++ backend/src/main/java/br/com/webgis/imovel/ImovelService.java
@@ -54,4 +54,6 @@ public class ImovelService{
 		}
 		repository.deleteById(id);
 	}
+
+	
 }
---
+++ backend/src/main/java/br/com/webgis/proprietario/Proprietario.java
@@ -0,0 +1,45 @@
+package br.com.webgis.proprietario;
+
+import java.time.OffsetDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+public class Proprietario {
+    
+@PrePersist
+	void aoCriar(){
+		this.criadoEm = OffsetDateTime.now();
+		this.atualizadoEm = this.criadoEm;
+	}
+
+	@PreUpdate
+	void aoAtualizar(){
+		this.atualizadoEm = OffsetDateTime.now();
+	}
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @NotBlank
+    @Size(max = 120)
+    @Column(length = 120)
+    public String nome;
+
+    @Column(name = "criado_em")
+	public OffsetDateTime criadoEm;
+
+	@Column(name = "atualizado_em")
+	public OffsetDateTime atualizadoEm;
+}
